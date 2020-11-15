b4654580b2e8101bc4fa3a1a9f5edd2d51da6c6c
Thoosk <thomas.skant@gmx.at>
2020-11-15 14:57:17 +0100

    add random and black color function

--- etch_a_sketch.js
+++ etch_a_sketch.js
@@ -7,19 +7,26 @@ size.addEventListener("click", resize);
 let resetButton = document.querySelector("#reset");
 resetButton.addEventListener("click", reset);
 
+let rndColorButton = document.querySelector("#rndColor");
+rndColorButton.addEventListener("click", randomColor);
+
+let blackColorButton = document.querySelector("#blackColor");
+blackColorButton.addEventListener("click", blackColor);
+
 setInitial();
 let containerArray = Array.from(document.querySelectorAll(".single-block"));
 
-//adding the class hovering to each element of of the container
+//adding the class default_hover to each element of of the container
 containerArray.forEach((cont) => {
   cont.addEventListener("mouseenter", () => {
-    cont.classList.add("hovering");
+    cont.classList.add("default_hover");
   });
 });
 
 //RESET
 function reset() {
   containerArray.forEach((cont) => {
+    cont.style.backgroundColor = "";
     cont.setAttribute("class", "single-block");
   });
 }
@@ -40,6 +47,7 @@ function resize() {
   });
   container.style.gridTemplateColumns = `repeat(${newSize}, 1fr)`;
   container.style.gridTemplateRows = `repeat(${newSize}, 1fr)`;
+
   for (let i = 0; i < newSize * newSize; i++) {
     grid = document.createElement("div");
     grid.classList.add("single-block");
@@ -49,15 +57,36 @@ function resize() {
   containerArray = Array.from(document.querySelectorAll(".single-block"));
   containerArray.forEach((cont) => {
     cont.addEventListener("mouseenter", () => {
-      cont.classList.add("hovering");
+      cont.classList.add("default_hover");
     });
   });
   // console.log(containerArray);
 }
 
-// console.log(containerArray);
+/// DIFFERENT COLORS
 
-// e.classList.add("hovering");
+// RANDOM
+function randomColor() {
+  reset();
+  containerArray.forEach((cont) => {
+    cont.addEventListener("mouseenter", () => {
+      const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+      cont.style.backgroundColor = `#${randomColor}`;
+    });
+  });
+}
+
+// BLACK
+function blackColor() {
+  reset();
+  containerArray.forEach((cont) => {
+    cont.addEventListener("mouseenter", () => {
+      cont.style.backgroundColor = "";
+    });
+  });
+}
+
+// e.classList.add("default_hover");
 
 // grid.addEventListener('mouseenter', func) { (when mouse hovers over item
 //
